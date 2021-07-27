f6932aecf3955af2a9537a05a7a25f92e1968c09
Jorge Jaime-Rivera <jaimeriverajorge@gmail.com>
2021-07-27 15:05:05 -0500

    created test.py file

--- landmark_detection/src/test.py
+++ landmark_detection/src/test.py
@@ -13,6 +13,8 @@ from model import FaceKeypointResNet50
 model = FaceKeypointResNet50(
     pretrained=False, requires_grad=False).to(config.DEVICE)
 
+resize = 224
+
 # just change the image and model names if wish to use a different one
 # most lobes
 img_name = 'IA-MG245-W-A'
@@ -20,7 +22,10 @@ img_name = 'IA-MG245-W-A'
 #img_name = 'IL-SF003-E-A'
 # medium lobes
 #img_name = 'MO-MG404-N-B'
-model_name = 'landmark_output_long_data_with_1s'
+model_name = 'lobe_landmark_output_filled_with_1s_1000_epochs_lr001'
+
+# true test image
+#img_name = 'IA-MG244-S-C'
 
 # load the model checkpoint
 # in this case, using the model filled with 1s, trained for
@@ -34,10 +39,17 @@ model.eval()
 # load the image into the model
 
 with torch.no_grad():
-
+    # for getting an image that was used for training
     image = cv2.imread(f'../input/training/{img_name}.jpg')
+
+    # for getting an image that is brand new (true test)
+    # coming from the small res images, since the data
+    # filled with 1s was trained on the small res
+    # image = cv2.imread(
+    #    f'../../../data-extraction/oak_images_small/{img_name}.jpg')
+
     im_h, im_w, im_c = image.shape
-    image = cv2.resize(image, (224, 224))
+    image = cv2.resize(image, (resize, resize))
     orig_frame = image.copy()
     orig_h, orig_w, c = orig_frame.shape
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -52,12 +64,18 @@ outputs = outputs.cpu().detach().numpy()
 outputs = outputs.reshape(-1, 2)
 
 keypoints = outputs
+keypoints = keypoints * [im_w / resize, im_h / resize]
 orig_frame = cv2.resize(orig_frame, (im_w, im_h))
 plt.imshow(orig_frame)
 for p in range(keypoints.shape[0]):
     plt.plot(int(keypoints[p, 0]), int(keypoints[p, 1]), 'r.')
+    # cv2.circle(orig_frame, (int(keypoints[p, 0]), int(
+    #    keypoints[p, 1])), 1, (0, 0, 255), -1, cv2.LINE_AA)
 
+#orig_frame = cv2.resize(orig_frame, (im_w, im_h))
+#cv2.imshow('landmark points:', orig_frame)
+# cv2.waitKey(0)
 plt.savefig(f"{config.OUTPUT_PATH}/test_{img_name}.png")
-
+plt.show()
 
 # cv2.destroyAllWindows()
